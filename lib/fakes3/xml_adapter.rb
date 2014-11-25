3419ddcce57fe0ba2e383ec784c5239f83c0a310
Felix Hageloh <felix.hageloh@gmail.com>
2014-11-25 21:54:50 +0100

    WIP for combining chunked uploads

--- lib/fakes3/xml_adapter.rb
+++ lib/fakes3/xml_adapter.rb
@@ -189,6 +189,25 @@ module FakeS3
         result.ETag("\"#{object.md5}\"")
       }
       output
-    end    
+    end
+
+    # <CompleteMultipartUploadResult>
+    #   <Location>http://Example-Bucket.s3.amazonaws.com/Example-Object</Location>
+    #   <Bucket>Example-Bucket</Bucket>
+    #   <Key>Example-Object</Key>
+    #   <ETag>"3858f62230ac3c915f300c664312c11f-9"</ETag>
+    # </CompleteMultipartUploadResult>
+    def self.complete_multipart_result(object)
+      output = ""
+      xml = Builder::XmlMarkup.new(:target => output)
+      xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
+      xml.CompleteMultipartUploadResult { |result|
+        result.Location("TODO: implement")
+        result.Bucket("TODO: implement")
+        result.Key(object.name)
+        result.ETag("\"#{object.md5}\"")
+      }
+      output
+    end
   end
 end
